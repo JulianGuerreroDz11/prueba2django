7d70084a5869e0e23b8d76c15395dc78f9be1de1
Julian Guerrero <julianchog11@outlook.com>
2022-10-05 09:40:10 -0500

    create in  template: var, metods,list, clycles and condicionals
    Implementar variables, uso de metodos, listas(arreglos) con ciclos y/o condicionales

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -7,9 +7,15 @@ def saludo(request): #Primera vista
     #Leer una plantilla de forma manual
     #Abrir ubicacion archivo
     doc_externo=open("C:/Users/pc/Desktop/GitHub/prueba2 django/Proyecto1/Proyecto1/plantillas/miplanquilla.html")
+    #Variables
+    nombre="Julian"
+    apellido="Guerrero"
+    fecha_actual=datetime.datetime.now()
+
     plt=Template(doc_externo.read()) #Creacion objeto template
     doc_externo.close() #Cerramos documento para optimizar recursos
-    ctx= Context()  #Creacion de contexto (variablos-obj)
+    ctx= Context({"nombre_persona":nombre,"apellido_persona":apellido,"hora_actual":fecha_actual,
+    "temas":["Plantillas","Modelos","Formularios","Visitas","Despliegue"]})  #Creacion de contexto (variablos-obj)
     documento=plt.render(ctx)   #Renderizado
     return HttpResponse(documento)
 
@@ -17,5 +23,5 @@ def despedida(request):
     return HttpResponse("Hasta luego alumnos de Django")
 
 def dameFecha(request):
-    fecha_actual=datetime.datetime.now()
+    
     return HttpResponse("La hora es %s" %fecha_actual)
