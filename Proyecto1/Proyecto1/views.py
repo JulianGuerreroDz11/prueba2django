121e034d1b1c7ef3fdd7560910e9040744ce0c7d
Julian Guerrero <julianchog11@outlook.com>
2022-10-05 09:03:05 -0500

    create new  manual template
    Creacion de nueva plantilla usando instruccionar "manual"

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,8 +1,17 @@
+from pipes import Template
 from django.http import HttpResponse
 import datetime
+from django.template import Template,Context
 
 def saludo(request): #Primera vista
-    return HttpResponse("Hola alumnos esta es la nuestra primera pagina de Django")
+    #Leer una plantilla de forma manual
+    #Abrir ubicacion archivo
+    doc_externo=open("C:/Users/pc/Desktop/GitHub/prueba2 django/Proyecto1/Proyecto1/plantillas/miplanquilla.html")
+    plt=Template(doc_externo.read()) #Creacion objeto template
+    doc_externo.close() #Cerramos documento para optimizar recursos
+    ctx= Context()  #Creacion de contexto (variablos-obj)
+    documento=plt.render(ctx)   #Renderizado
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse("Hasta luego alumnos de Django")
